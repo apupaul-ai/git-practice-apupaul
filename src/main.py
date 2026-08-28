84213381f2cccc0689202765fb2e164a17be19b1
Apu Paul <apupaul7666100@gmail.com>
2026-08-28 22:57:47 +0600

    Add multiply function to calculator

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Name: Apu Paul")
 print("Today's date:", date.today())
 
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+print("Multiplication:", multiply(5, 3))
